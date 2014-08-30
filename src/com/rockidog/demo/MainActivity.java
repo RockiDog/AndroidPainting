713709e83d27c7fb6f2243ed4e98eea977cb4b8d
Wang Ru <rockidog@live.com>
2014-08-30 09:51:56 +0800

    First add

---
+++ src/com/rockidog/demo/MainActivity.java
@@ -0,0 +1,20 @@
+package com.rockidog.demo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startPainting(View view) {
+        Intent intent = new Intent(this, PaintingActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ src/com/rockidog/demo/PaintingView.java
@@ -0,0 +1,94 @@
+package com.rockidog.demo;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class PaintingView extends View {
+    private static final float TOUCH_TOLERANCE = 4;
+    private float mX;
+    private float mY;
+
+    private Bitmap mBitmap;
+    private Canvas mCanvas;
+    private Path mPath;
+    public Paint mPaint;
+
+    public PaintingView(Context context) {
+        super(context);
+        mPath = new Path();
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setDither(true);
+        mPaint.setColor(Color.WHITE);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeJoin(Paint.Join.ROUND);
+        mPaint.setStrokeCap(Paint.Cap.ROUND);
+        mPaint.setStrokeWidth(6);
+    }
+
+    @Override
+    protected void onSizeChanged(int width, int height, int oldWidth, int oldHeight) {
+        super.onSizeChanged(width, height, oldWidth, oldHeight);
+        mBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        mCanvas = new Canvas(mBitmap);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawColor(Color.DKGRAY);
+        canvas.drawBitmap(mBitmap, 0, 0, null);
+        canvas.drawPath(mPath, mPaint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+        switch(event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                touchDown(x, y);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                touchMove(x, y);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_UP:
+                touchUp();
+                invalidate();
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+
+    private void touchDown(float x, float y) {
+        mPath.reset();
+        mPath.moveTo(x, y);
+        mX = x;
+        mY = y;
+    }
+
+    private void touchMove(float x, float y) {
+        final float dx = Math.abs(x - mX);
+        final float dy = Math.abs(y - mY);
+        if (dx > TOUCH_TOLERANCE || dy > TOUCH_TOLERANCE) {
+            mPath.quadTo(mX, mY, (x + mX) / 2, (y + mY) / 2);
+            mX = x;
+            mY = y;
+        }
+    }
+
+    private void touchUp() {
+        mPath.lineTo(mX, mY);
+        mCanvas.drawPath(mPath, mPaint);
+        mPath.reset();
+    }
+}
